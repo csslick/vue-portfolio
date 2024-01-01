41ff892fe44f9932be6e0cf4d523db63518a38e1
csslick <digitone@naver.com>
2024-01-01 23:12:54 +0900

    setup

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template lang="">
+  <div>
+    <h1 class='display-1'>App</h1>
+    <button class="btn btn-primary">primary</button>
+  </div>
+</template>
+<script>
+export default {
+  
+}
+</script>
+<style lang="scss">
+  
+  div {
+    h1 {
+      color: gold;
+      &:hover { color: red;}
+    }
+  }
+</style>
