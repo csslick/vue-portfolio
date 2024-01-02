113f63652e3b09fbb00d77c570963eb26fb8e238
csslick <digitone@naver.com>
2024-01-02 13:51:18 +0900

    title

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,80 @@
-<template lang="">
-  <div>
-    <h1 class='display-1'>App</h1>
-    <button class="btn btn-primary">primary</button>
-  </div>
+<template>
+  <section id="title">
+    <div class='container p-5'>
+      <nav>
+        <a href="#">home</a>
+        <a href="#">portfolio</a>
+      </nav>
+      <div class="detail row">
+        <figure class="image col-md-6">
+          <img src="./assets/images/title.png" alt="title">
+        </figure>
+        <div class="text col-md-6">
+          <h2 class="text-light">Hello,<br>I'm Doolee</h2>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 <script>
 export default {
-  
+  name: 'App',
 }
 </script>
+
 <style lang="scss">
-  
-  div {
-    h1 {
-      color: gold;
-      &:hover { color: red;}
+  #title {
+    background: #333;
+    text-align: center;
+    height: 100dvh;
+
+    & > div {
+      display: flex;
+      flex-direction: column;
+      height: 100%;
+    }
+    .detail {
+      display: flex;
+      flex-direction: row-reverse;
+      height: 100%;
+      justify-content: space-around;
+      align-items: center;
+      .text h2 { font-size: 4em; }
+    }
+
+    @media screen and (max-width: 768px) {
+      .detail {
+        // border: 2px solid red;
+        display: flex;
+        height: 100%;
+        flex-direction: column;
+        align-items: space-around;
+        .text h2 { font-size: 2em; }
+      }
+    }
+
+    nav {
+      display: flex;
+      gap: 3em;
+      justify-content: center;
+
+      a{
+        color: #fff;
+        text-decoration: none;
+        text-transform: uppercase;
+      }
+    }
+
+    img {
+      width: 100%;
+      padding: 40px;
+    }
+
+    .text, .image {
+      display: flex;
+      justify-content: center;
+      align-items: center;
     }
   }
+
 </style>
