1ebc8cf04963522082361a038f1215ae53171b13
csslick <digitone@naver.com>
2024-01-02 19:53:54 +0900

    about 추가

---
+++ src/components/About.vue
@@ -0,0 +1,21 @@
+<template>
+  <section id="about">
+    <h1>About</h1>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'About',
+  data() {
+    return {
+
+    }
+  }
+  
+}
+</script>
+
+<style lang="scss">
+  
+</style>
